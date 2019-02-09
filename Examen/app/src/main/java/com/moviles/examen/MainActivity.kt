7febcf442e2e42911c1d125440888a860ca41e7b
Ronald <ronald_96c@hotmil.com>
2019-02-09 04:57:53 -0500

    fgh

--- Examen/app/src/main/java/com/moviles/examen/MainActivity.kt
+++ Examen/app/src/main/java/com/moviles/examen/MainActivity.kt
@@ -17,7 +17,10 @@ class MainActivity : AppCompatActivity() {
         btn_listar.setOnClickListener {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of c066d74... back en la web
 =======
 >>>>>>> parent of c066d74... back en la web
 =======
